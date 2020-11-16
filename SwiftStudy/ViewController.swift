8247b25a775f04492710285cc66b49a375888d62
Choi-YeonJin <yeonjin9721@naver.com>
2020-11-17 08:52:29 +0900

    image Change & 확대,축소

--- SwiftStudy/ViewController.swift
+++ SwiftStudy/ViewController.swift
@@ -8,17 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var isZoom = false
+    var imgOn : UIImage?
+    var imgOff : UIImage?
 
-    @IBOutlet weak var lbHello: UILabel!
-    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var btnResize: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
-    @IBAction func btnSubmit(_ sender: UIButton) {
-        lbHello.text = "Hello! " + txtName.text!
+        imgOn = UIImage(named: "lightOn.png")
+        imgOff = UIImage(named: "lightOff.png")
+        
+        imgView.image = imgOn
+        
+        
     }
     
+    @IBAction func btnResizeImage(_ sender: UIButton) {
+        let scale : CGFloat = 2.0
+        var newWidth:CGFloat, newHeight:CGFloat
+        
+        if(isZoom){ //true - 축소
+            newWidth = imgView.frame.width/scale
+            newHeight = imgView.frame.height/scale
+            imgView.frame.size = CGSize(width: newWidth, height: newHeight)
+            btnResize.setTitle("확대" , for: .normal)
+            
+        }else{ // false - 확대
+            newWidth = imgView.frame.width*scale
+            newHeight = imgView.frame.height*scale
+            imgView.frame.size = CGSize(width: newWidth, height: newHeight)
+            btnResize.setTitle("축소" , for: .normal)
+        }
+        isZoom = !isZoom
+    }
+    
+    @IBAction func switchImgOnOff(_ sender: UISwitch) {
+        if(sender.isOn){
+            imgView.image = imgOn
+        }else{
+            imgView.image = imgOff
+        }
+    }
 }
 
